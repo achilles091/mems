91f28e4915528b1f8daedd620b1e3c78b1ac7f5d
achilles091 <113716330+achilles091@users.noreply.github.com>
2023-02-27 12:29:07 -0500

    Update mems.py

--- mems.py
+++ mems.py
@@ -1,17 +1,5 @@
 # -*- coding: utf-8 -*-
 
-pip3 install board
-pip3 install adafruit_bme280
-sudo pip install RPi.bme280
-sudo apt-get install i2c-tools python-pip
-sudo apt-get update
-sudo apt-get install git build-essential i2c-tools
-
-sudo apt update
-sudo apt install libretech-gpio libretech-dtoverlay
-sudo ldto enable i2c-ao
-sudo ldto enable i2c-b
-
 import board
 from adafruit_bme280 import basic as adafruit_bme280
 i2c = board.I2C()
