53564536c1bf2576576e1c38f6c53ecfa44b22b7
achilles091 <113716330+achilles091@users.noreply.github.com>
2023-02-27 12:28:52 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pip3 install board
+pip3 install adafruit_bme280
+sudo pip install RPi.bme280
+sudo apt-get install i2c-tools python-pip
+sudo apt-get update
+sudo apt-get install git build-essential i2c-tools
+
+sudo apt update
+sudo apt install libretech-gpio libretech-dtoverlay
+sudo ldto enable i2c-ao
+sudo ldto enable i2c-b
